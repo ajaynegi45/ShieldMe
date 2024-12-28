2b6d260fcb1cfed037e59371ae80757c5875a0ee
Yash-Raj07 <sah99624@gmail.com>
2024-12-28 19:14:16 +0530

    init commit

--- Authentication/src/main/java/com/shieldme/authentication/AuthenticationApplication.java
+++ Authentication/src/main/java/com/shieldme/authentication/AuthenticationApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthenticationApplication {
 
 
+
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationApplication.class, args);
     }
