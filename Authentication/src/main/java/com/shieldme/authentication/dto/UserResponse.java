c2d640a1c91670b42bc69589b001a6f203eff46e
ajaynegi45 <ajaynegi3345@icloud.com>
2024-12-29 00:11:05 +0530

    Update

--- Authentication/src/main/java/com/shieldme/authentication/dto/UserResponse.java
+++ Authentication/src/main/java/com/shieldme/authentication/dto/UserResponse.java
@@ -1,7 +1,6 @@
 package com.shieldme.authentication.dto;
 
 import com.shieldme.authentication.entity.ProfileImage;
-import org.bson.types.ObjectId;
 
 public record UserResponse(
         String userId,
--- Authentication/src/main/java/com/shieldme/authentication/service/ProfileUpdateService.java
+++ Authentication/src/main/java/com/shieldme/authentication/service/ProfileUpdateService.java
@@ -1,14 +1,13 @@
 package com.shieldme.authentication.service;
 
-import com.shieldme.authentication.dto.UpdateRequest;
 import com.shieldme.authentication.dto.UserResponse;
-import com.shieldme.authentication.entity.PasswordDetails;
-import com.shieldme.authentication.entity.ProfileImage;
 import com.shieldme.authentication.entity.User;
 import com.shieldme.authentication.config.PasswordEncoder;
 import com.shieldme.authentication.exception.UserNotFoundException;
 import com.shieldme.authentication.repository.UserRepository;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,45 +16,38 @@ import java.io.IOException;
 @Service
 public class ProfileUpdateService {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProfileUpdateService.class);
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
     public ProfileUpdateService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public UserResponse updateUserProfile(ObjectId userId, String name, String email, String password, MultipartFile profileImage) throws IOException, IOException {
+    public UserResponse updateUserProfile(ObjectId userId, String name, String email, String password, MultipartFile profileImage) throws IOException {
 
-        // Fetch the user
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new UserNotFoundException("User not found"));
+                .orElseThrow(() -> {
+                    logger.warn("User not found for userId: {}", userId);
+                    return new UserNotFoundException("User not found.");
+                });
 
-        // Update fields
         if (name != null) user.setName(name);
         if (email != null) user.setEmail(email);
-        if (password != null && !password.isEmpty()) {
-            String encodedPassword = passwordEncoder.bCryptPasswordEncoder().encode(password);
-            user.getPasswordDetails().setPassword(encodedPassword);
-        }
+        if (password != null && !password.isEmpty()) user.getPasswordDetails().setPassword(passwordEncoder.bCryptPasswordEncoder().encode(password));
 
-        // Process the profile image
         if (profileImage != null) {
-            if (!profileImage.getContentType().startsWith("image/")) {
+            if (!profileImage.getContentType().startsWith("image/"))
                 throw new IllegalArgumentException("Invalid file type. Only images are allowed.");
-            }
 
-            if (profileImage.getSize() > 1024 * 1024) { // 1MB limit
+            if (profileImage.getSize() > 1024 * 1024) // profileImage size should be less than 1MB
                 throw new IllegalArgumentException("Image size should not exceed 1MB!");
-            }
 
             user.getProfileImage().setImageName(profileImage.getOriginalFilename());
             user.getProfileImage().setImageType(profileImage.getContentType());
             user.getProfileImage().setImageData(profileImage.getBytes());
         }
-
         userRepository.save(user);
-
         return new UserResponse(
                 user.getUserId().toString(),
                 user.getName(),
